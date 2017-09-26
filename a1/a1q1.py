48dac99c89eec445c8c3e9a54e67f311a7dced7a
Azoacha Forcheh <azoachaforcheh@v1020-wn-126-115.campus-dynamic.uwaterloo.ca>
2017-09-26 14:03:04 -0400

    Added parts of design recipe and assertions to a1q1

--- a1/a1q1.py
+++ a1/a1q1.py
@@ -14,18 +14,61 @@
 # AppointmentBook is dictionary of appointments, where the key is a tuple (
 #   date, time), and value is the purpose of the appointment
 class AppointmentBook:
-    # create an empty appointment book
+    """
+    Fields: appts ((dictof (tuple Int Float) Str)),
+    Requires: 1 <= date <=365, where date is the first element of the keys of
+                    appts
+              8.0 <= time <= 16.5, where time is the second element of the keys
+                    of appts
+    """
+
+    # AppointmentBook() produces an empty AppointmentBook object with an empty
+    #   dictionary of appointments appts, with the keys being a tuple of the
+    #   date and time of the appointment and the values being the purpose, i.e.
+    #   an entry in appts would be of format (date, time):purpose
+    # __init__: None -> AppointmentBook
     def __init__(self):
         self.appts = {}
 
-    #determines if an appointment exists for the date and time specified
+    # isAppointment(self, apptDate, apptTime) determines if an appointment
+    #   exists for the date apptDate and time apptTime specified and returns
+    #   True if there does, and False if there doesn't.
+    # isAppointment: AppointmentBook Int Float -> Bool
+    # Requires: 1 <= apptDate <=365
+    #           8.0 <= apptTime <= 16.5
     def isAppointment(self, apptDate, apptTime):
+        # checking the type and values of the parameters
+        assert type(apptDate) is int, \
+            "Invalid Type: apptDate is not an integer"
+        assert type(apptTime) is float, \
+            "Invalid Type: apptTime is not a float"
+        assert (apptDate >= 1 and apptDate <= 365), \
+            "Invalid Value: apptDate should be between 1 and 365, inclusive"
+        assert (apptTime >= 8.0 and apptTime <= 16.5), \
+            "Invalid Value: apptTime should be between 8.0 and 16.5, inclusive"
+
         appt_to_check = (apptDate, apptTime)
         return appt_to_check in self.appts
 
-    #inserts the appointment for the date, time and purpose specified,
-    #as long as it does not conflict with an existing appointment
+    # makeAppointment(self,apptDate,apptTime,purpose) inserts the appointment
+    #   for the date apptDate, time apptTime and purpose specified (as long as
+    #   it does not conflict with an existing appointment), and returns True if
+    #   successfully added and False if not.
+    # Effects: Mutates self.
+    # makeAppointment: AppointmentBook Int Float Str -> Bool
+    # Requires: 1 <= apptDate <=365
+    #           8.0 <= apptTime <= 16.5
     def makeAppointment(self, apptDate, apptTime, purpose):
+        # checking the type and values of the parameters
+        assert type(apptDate) is int, \
+            "Invalid Type: apptDate is not an integer"
+        assert type(apptTime) is float, \
+            "Invalid Type: apptTime is not a float"
+        assert (apptDate >= 1 and apptDate <= 365), \
+            "Invalid Value: apptDate should be between 1 and 365, inclusive"
+        assert (apptTime >= 8.0 and apptTime <= 16.5), \
+            "Invalid Value: apptTime should be between 8.0 and 16.5, inclusive"
+
         appt_check = not self.isAppointment(apptDate, apptTime)
         if appt_check:
             self.appts[(apptDate, apptTime)] = purpose
@@ -33,6 +76,16 @@ class AppointmentBook:
 
     #deletes the appointment for the date and time specified
     def cancelAppointment(self, apptDate, apptTime):
+        # checking the type and values of the parameters
+        assert type(apptDate) is int, \
+            "Invalid Type: apptDate is not an integer"
+        assert type(apptTime) is float, \
+            "Invalid Type: apptTime is not a float"
+        assert (apptDate >= 1 and apptDate <= 365), \
+            "Invalid Value: apptDate should be between 1 and 365, inclusive"
+        assert (apptTime >= 8.0 and apptTime <= 16.5), \
+            "Invalid Value: apptTime should be between 8.0 and 16.5, inclusive"
+
         appt_check = self.isAppointment(apptDate, apptTime)
         if appt_check:
             self.appts.pop((apptDate, apptTime))
@@ -40,13 +93,42 @@ class AppointmentBook:
 
     # retrieves the purpose of the appointment if one exists
     def checkAppointment(self, apptDate, apptTime):
+        # checking the type and values of the parameters
+        assert type(apptDate) is int, \
+            "Invalid Type: apptDate is not an integer"
+        assert type(apptTime) is float, \
+            "Invalid Type: apptTime is not a float"
+        assert (apptDate >= 1 and apptDate <= 365), \
+            "Invalid Value: apptDate should be between 1 and 365, inclusive"
+        assert (apptTime >= 8.0 and apptTime <= 16.5), \
+            "Invalid Value: apptTime should be between 8.0 and 16.5, inclusive"
+
         appt_check = self.isAppointment(apptDate, apptTime)
         if appt_check:
             return self.appts[(apptDate, apptTime)]
-        return ""
+        return None
 
     #change the date or time for an appointment
     def changeAppointment(self, oldDate, oldTime, newDate, newTime):
+        # checking the type and values of the parameters
+        assert type(oldDate) is int, \
+            "Invalid Type: oldDate is not an integer"
+        assert type(oldTime) is float, \
+            "Invalid Type: oldTime is not a float"
+        assert (oldDate >= 1 and oldDate <= 365), \
+            "Invalid Value: oldDate should be between 1 and 365, inclusive"
+        assert (oldTime >= 8.0 and oldTime <= 16.5), \
+            "Invalid Value: oldTime should be between 8.0 and 16.5, inclusive"
+
+        assert type(newDate) is int, \
+            "Invalid Type: newDate is not an integer"
+        assert type(newTime) is float, \
+            "Invalid Type: newTime is not a float"
+        assert (newDate >= 1 and newDate <= 365), \
+            "Invalid Value: newDate should be between 1 and 365, inclusive"
+        assert (newTime >= 8.0 and newTime <= 16.5), \
+            "Invalid Value: newTime should be between 8.0 and 16.5, inclusive"
+
         old_appt_check = not self.isAppointment(oldDate, oldTime)
         new_appt_check = self.isAppointment(newDate, newTime)
         appt_check = not(old_appt_check or new_appt_check)
@@ -66,6 +148,12 @@ class AppointmentBook:
 
     # retrieves all the appointment on the given date
     def getAppointmentsByDate(self, date):
+        # checking the type and values of the parameters
+        assert type(date) is int, \
+            "Invalid Type: date is not an integer"
+        assert (date >= 1 and date <= 365), \
+            "Invalid Value: date should be between 1 and 365, inclusive"
+
         appt_keys = [key for key in self.appts if key[0] == date]
         appt_list = []
 
